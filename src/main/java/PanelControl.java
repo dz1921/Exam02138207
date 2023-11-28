d81f7774a54fc0f27009e05d6c17c3d7d5da1665
Denis Zahorovsky <dz1921@ic.ac.uk>
2023-11-28 11:23:41 +0000

    v2.0

--- src/main/java/PanelControl.java
+++ src/main/java/PanelControl.java
@@ -12,8 +12,8 @@ public class PanelControl extends JPanel {
         setLayout(new GridLayout(2,1));
 
         Patient pat1 = new Patient("Daphne Von Oram", "https://martinh.netfirms.com/BIOE60010/DaphneVonOram.jpg", 62);
-        BPExamination mybp = new BPExamination(LocalDate.now(), 130,70, "ST");
-        MRIExamination mymri = new MRIExamination(LocalDate.now(), "https://martinh.netfirms.com/BIOE60010/mri1.jpg", 2);
+        BPExamination mybp = new BPExamination(LocalDate.of(2023,9,23), 130,70, "ST");
+        MRIExamination mymri = new MRIExamination(LocalDate.of(2023,9,15), "https://martinh.netfirms.com/BIOE60010/mri1.jpg", 2);
         pat1.assignBPExamination(mybp);
         pat1.assignMRIExamination(mymri);
 
@@ -22,8 +22,8 @@ public class PanelControl extends JPanel {
 
 
         Patient pat2 = new Patient("Sebastian Compton", "https://martinh.netfirms.com/BIOE60010/SebastianCompton.jpg", 31);
-        BPExamination mybp2 = new BPExamination(LocalDate.now(), 150, 80, "VST");
-        MRIExamination mymri2 = new MRIExamination(LocalDate.now(), "https://martinh.netfirms.com/BIOE60010/mri2.jpg", 4);
+        BPExamination mybp2 = new BPExamination(LocalDate.of(2023,11,20), 150, 80, "VST");
+        MRIExamination mymri2 = new MRIExamination(LocalDate.of(2023,11,19), "https://martinh.netfirms.com/BIOE60010/mri2.jpg", 4);
         pat2.assignMRIExamination(mymri2);
         pat2.assignBPExamination(mybp2);
         doctorsPanel.addPatient(pat2);
